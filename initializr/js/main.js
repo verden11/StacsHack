26e2065a03d265bc023992582d56e8a44b683cbb
= <guest73@pc3-014-l.cs.st-andrews.ac.uk>
2015-02-21 19:52:28 +0000

    script moved

--- initializr/js/main.js
+++ initializr/js/main.js
@@ -1,1 +1,14 @@
-
+function unhide(para){
+          var name = document.getElementById(para).value;
+          var l = name.length;
+          var num = para.slice(-1);
+          var tickMark = "tc" + num;
+          var xMark = "tcx" + num;
+          if (name.search(/[^A-Za-z\s]/) == -1 && l > 0) {
+             document.getElementById(tickMark).style.display = "";
+             document.getElementById(xMark).style.display = "none";
+          }else{  
+            document.getElementById(xMark).style.display = "";
+            document.getElementById(tickMark).style.display = "none";
+          }
+        }
