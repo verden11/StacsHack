321c90209c6d2db9ecdf490b0f3b3b19e324f614
= <guest73@pc3-014-l.cs.st-andrews.ac.uk>
2015-02-22 00:40:33 +0000

    addes step2.html

--- initializr/js/main.js
+++ initializr/js/main.js
@@ -12,3 +12,18 @@ function unhide(para){
             document.getElementById(tickMark).style.display = "none";
           }
         }
+
+
+function allowDrop(ev) {
+    ev.preventDefault();
+}
+
+function drag(ev) {
+    ev.dataTransfer.setData("text", ev.target.id);
+}
+
+function drop(ev) {
+    ev.preventDefault();
+    var data = ev.dataTransfer.getData("text");
+    ev.target.appendChild(document.getElementById(data));
+}
