00d78eb32c8855c00835e1821035a2154a0cc9ce
= <guest73@pc3-014-l.cs.st-andrews.ac.uk>
2015-02-22 07:07:52 +0000

    minor bug over 9000

--- js/main.js
+++ js/main.js
@@ -27,3 +27,20 @@ function drop(ev) {
     var data = ev.dataTransfer.getData("text");
     ev.target.appendChild(document.getElementById(data));
 }
+
+function passStr(para,para2){
+  var name = document.getElementById(para).value;
+  var name2 = document.getElementById(para2).value;
+  var l = name.length;
+  var num = para.slice(-1);
+  var tickMark = "tc" + num;
+  var xMark = "tcx" + num;
+  if (name.search(/[^A-Za-z\s]/) != -1 && l > 6 && name == name2) {
+             document.getElementById(tickMark).style.display = "";
+             document.getElementById(xMark).style.display = "none";
+          }else{  
+            document.getElementById(xMark).style.display = "";
+            document.getElementById(tickMark).style.display = "none";
+          }
+  
+}
